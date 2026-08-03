66bbf6bd500e50e7a5d8b2697f6613f691020369
Walter Moreira <walter@waltermoreira.net>
2026-08-02 23:14:13 -0500

    Remove unnecessary script

--- home-manager.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
-MACHINE=$("${HOME}/.nix-profile/bin/nix" eval --raw "path:$(pwd)#machineName")
-"${HOME}/.nix-profile/bin/nix" run home-manager/master switch -- -b backup --flake ".#$MACHINE"
