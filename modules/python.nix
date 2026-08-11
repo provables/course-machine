d1e353b08a3c76791d8630674517c1b635b5c17b
Walter Moreira <walter@waltermoreira.net>
2026-08-11 13:19:54 -0500

    Add python libraries

--- modules/python.nix
+++ modules/python.nix
@@ -3,6 +3,12 @@
     (python314.withPackages (p: [
       p.jupyter
       p.requests
+      p.pandas
+      p.numpy
+      p.scikit-learn
+      p.matplotlib
+      p.tensorflow
+      p.keras
     ]))
     black
   ];
