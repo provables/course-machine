df7c93f5ac71331747ea10a40e3f8d5f102361d7
Walter Moreira <walter@waltermoreira.net>
2026-08-26 14:24:05 -0500

    Add comments

--- extraPackages.nix
+++ extraPackages.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   course.extraPackages = with pkgs; [
     # Add packages from Nixpkgs
+    # Search available packages at: https://search.nixos.org/packages
+    #
     # For example:
     cowsay
   ];
--- extraPythonPackages.nix
+++ extraPythonPackages.nix
@@ -1,6 +1,11 @@
 {
   course.extraPythonPackages = p: with p; [
-    # Add Python packages from https://search.nixos.org
+    # Add Python packages from Nixpkgs
+    # Search available packages at: https://search.nixos.org/packages?query=python313Packages
+    # Write below the name after the dot.
+    # For example, if one finds `python313Packages.sympy` in the above URL, then
+    # simply write below: `sympy`.
+    #
     # For example,
     sympy
   ];
